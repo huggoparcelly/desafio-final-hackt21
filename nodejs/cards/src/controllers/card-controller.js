9a2d6f3e8dd2d54edbcf3014b255aea7c0c5474e
Huggo Parcelly <h.parcelly@gmail.com>
2021-01-23 14:45:28 -0300

    Desafio final NodeJs

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -1,7 +1,88 @@
 const neDB = require('../configurations/database')
+const { response, request } = require('../configurations/express')
 const api = {}
 
+
 api.findAll = (request, response) => {
+    neDB.find({}, (exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de listar todos os cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
 }
+
+
+
+api.findById = (request, response) => {
+    neDB.findOne({_id}, (exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de listar um card!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
+}
+
+
+api.findPg = (request, response) => {
+    neDB.find({}).sort({ name: 1 }).exec((exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de listar todos os cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
+}
+
+
+api.save = (request, response) => {
+    const canonical = request.body
+    neDB.insert(canonical, (exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de criar um cards!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.status(201)
+        response.json(cards)
+    })
+}
+
+
+api.update = (request, response) => {
+    neDB.update({_id}, (exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de atualizar um card!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.status(200)
+        response.json(cards)
+    })
+}
+
+api.remove = (request, response) => {
+    neDB.remove({_id}, (exception, cards) => {
+        if (exception) {
+            const setence = 'Deu ruim na tentativa de deletar um card!'
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+        response.status(200)
+        response.json(cards)
+    })
+}
+
 
 module.exports = api
--- nodejs/cards/src/routes/card-route.js
+++ nodejs/cards/src/routes/card-route.js
@@ -3,5 +3,12 @@ const api = require('../controllers/card-controller')
 module.exports = (app) => {
     app.route('')
         .get(api.findAll)
+        .get(api.findById)
+        .get(api.findPg)
+        .post(api.save)
+        .put(api.update)
+        .delete(api.remove)
+        
+
 
 }
